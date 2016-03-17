08e380c5815bd6783c5db7175d89c8ef1848a6de
felipo <felipo@bff55f7b-2b04-4285-a55e-6601ad790590>
2016-03-17 18:33:55 +0000

    general website update, many new languages

--- www/ttt/difinoj.m4
+++ www/ttt/difinoj.m4
@@ -1,7 +1,7 @@
-define(`_VERSIO_', `3.01')
-define(`_BZ2_', `1100 kB')
+define(`_VERSIO_', `3.02')
+define(`_BZ2_', `1200 kB')
 
-define(`_VERSIO_WIN_', `2.00b')
+define(`_VERSIO_WIN_', `2.00c')
 define(`_EXE_', `8400 kB')
 
 define(`_EKRAN_LIGO_', `http://sourceforge.net/projects/klavaro/')
@@ -10,13 +10,10 @@ define(`_HOST_', `http://klavaro.sourceforge.net/top10')
 
 define(`_PROGRAMISTOJ_', `<blockquote><p>- Felipe E. F. de Castro <a href="contrib.html#autores">&lt; fefcas (a) gmail (p) com &gt;</a></p><p>- Petr Machata</p></blockquote>')
         
-define(`_DESEGNISTOJ_', `<blockquote><p>- David Prieto</p></blockquote>')
+define(`_DESEGNISTOJ_', `<blockquote><p>- David Prieto</p><p>- Szczepan Marchewka</p></blockquote>')
 
 define(`_DATO_', esyscmd(date +%Y-%m-%d))
 define(`_VALIDIGO_', `<p><a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance#"><img src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="XHTML 1.0 Strict" height="31" width="88" /></a><br /></p>')
 
 define(`_LINGVOJ_', include(`../../klavaro-code/po/LINGUAS'))
-
-define(`<br>', `<br />')
-define(`<hr>', `<hr />')
 
--- www/ttt/kunmeti_ttt.sh
+++ www/ttt/kunmeti_ttt.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 
 # Farotaj taskoj antaŭ ol procezi dosierojn elŝutitajn el www.transifex.com
-# # Forigi en la dosieroj 11, 21 e 31: <html>, <head>, ..., </div>, </body>, </html>. (Atentu por ne forigi la <title> !!!)
+# Aldoni la lingvo-kodon en la listo sube, kaj krei dosierujojn kun tiu kodo ĉi tie kaj en ../htdocs.
+# Forigi en la dosieroj 11, 21 e 31: <html>, <head>, ..., </div>, </body>, </html>. (Atentu por ne forigi la <title> !!!)
+# Aldoni la koncernatan lingvan ligon en xx/03a-lingvoj.htm kaj ../htdocs/index.html
 
 export M4PATH=`pwd`
 
-for SUBD in cs de eo eu en fr hu pt; do
+for SUBD in ca cs de el eo eu en fr gl hr hu lt pa pl pt ru; do
 
 	MAKRO="m4 -D _LINGVO_KODO_=${SUBD} difinoj.m4"
 
@@ -13,15 +15,21 @@ for SUBD in cs de eo eu en fr hu pt; do
 
 	cat ../xx/00-kapo.htm 11-klavaro.htm 02-chefmenuo.htm ../xx/03a-lingvoj.htm 03-krommenuo.htm ../xx/04-piedo.htm > index.html;
 	$MAKRO index.html > index.htm4;
+	sed -i -e "s/<br>/<br \/>/g" index.htm4 
+	sed -i -e "s/<hr>/<hr \/>/g" index.htm4 
 	mv index.htm4 index.html;
 
 	cat ../xx/00-kapo.htm 31-kontrib.htm 02-chefmenuo.htm ../xx/03a-lingvoj.htm 03-krommenuo.htm ../xx/04-piedo.htm > contrib.html;
 	$MAKRO contrib.html > contrib.htm4;
+	sed -i -e "s/<br>/<br \/>/g" contrib.htm4 
+	sed -i -e "s/<hr>/<hr \/>/g" contrib.htm4 
 	mv contrib.htm4 contrib.html;
 
 	if test ${SUBD} != "eu"; then 
 	  cat ../xx/00-kapo.htm 21-helpo.htm   02-chefmenuo.htm ../xx/03a-lingvoj.htm 03-krommenuo.htm ../xx/04-piedo.htm > help.html;
 	  $MAKRO help.html > help.htm4;
+	  sed -i -e "s/<br>/<br \/>/g" help.htm4 
+	  sed -i -e "s/<hr>/<hr \/>/g" help.htm4 
 	  mv help.htm4 help.html;
         fi
 
@@ -32,6 +40,8 @@ done
 cd en;
 cat ../xx/00-kapo.htm 41-traduko.htm 02-chefmenuo.htm ../xx/03a-lingvoj.htm 03-krommenuo.htm ../xx/04-piedo.htm > translation.html;
 m4 -D _LINGVO_KODO_=en difinoj.m4 translation.html > translation.htm4;
+sed -i -e "s/<br>/<br \/>/g" translation.htm4 
+sed -i -e "s/<hr>/<hr \/>/g" translation.htm4 
 mv translation.htm4 ../../htdocs/en/translation.html;
 rm translation.html
 cd ../;
